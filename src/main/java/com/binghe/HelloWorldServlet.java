a59c5bdf5a19e9ba7bfa66a2320154537f1d6358
binghe819 <binghe819@naver.com>
2021-04-17 15:53:57 +0900

    feat: servlet (with Tomcat) hello world 구현

---
+++ src/main/java/com/binghe/HelloWorldServlet.java
@@ -0,0 +1,34 @@
+package com.binghe;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("HelloWorldServlet init()");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws ServletException, IOException {
+        System.out.println("HelloWorldServlet doGet()");
+        PrintWriter out = resp.getWriter();
+        out.println("<html>"+
+            "<body>"+
+            "<h1 align=center>Hello World Servlet</h1>"+
+            "<br>"+
+            "</body>"+
+            "</html>");
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("HelloWorldServlet destroy()");
+    }
+}
